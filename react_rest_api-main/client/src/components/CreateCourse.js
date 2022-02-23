901e0d0b421f05769e0aa7d54aaaa9275d161b2c
Nick Huemmer <njhuemmer@gmail.com>
2022-02-23 14:46:31 -0500

    refactored CreateCourse form

--- react_rest_api-main/client/src/components/CreateCourse.js
+++ react_rest_api-main/client/src/components/CreateCourse.js
@@ -1,118 +1,127 @@
 // We will need to use useContext for this component OR convert it to a Class componet.
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Form from './Form';
 
-export default function CreateCourse() {
+export default class CreateCourse extends Component {
     
-    const [title, setTitle] = useState('');
-    const [description, setDescription] = useState('');
-    const [estimatedTime, setEstimatedTime] = useState('');
-    const [materialsNeeded, setMaterialsNeeded] = useState('');
-    const [errors, setErrors] = useState([]);
-    
+   state = { 
+        title:'',
+        description:'',
+        estimatedTime:'',
+        materialsNeeded:'',
+        errors:[]
+   }
 
-    let handleSubmit = async (e) => {
-        e.preventDefault();
-        console.log('cereal');
-        try {
-            const response = await fetch('http://localhost:5000/api/courses', {
-                method: 'POST',
-                body: JSON.stringify({
-                    title,
-                    description,
-                    estimatedTime,
-                    materialsNeeded,
-                }),
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            });
-            console.log(response);
-            console.log(title);
-            console.log('sausage');
-            const json = await response.json();
-            console.log(json);
-            console.log(errors);
-            if (json.errors) {
-                console.log('chicken in a biscuit');
-                setErrors(json.errors);
-                console.log('oatmeal');
-            } else {
-                setTitle('');
-                setDescription('');
-                setEstimatedTime('');
-                setMaterialsNeeded('');
-                setErrors([]);
-            }
-        } catch (err) {
-            console.error(err);
-            console.log('pancakes');
-        }
-    }
+    render() {
+        const { 
+            title, 
+            description,
+            estimatedTime,
+            materialsNeeded,
+            errors} = this.state
 
     return (
-            <div className="wrap">
-                <h2>Create Course</h2>
-                    { errors.length > 0 ? 
-                    <div className="validation--errors">
-                        <h3>Validation Errors</h3>
-                        <ul>
-                            <li>Please provide a value for "Title"</li>
-                            <li>Please provide a value for "Description"</li>
-                        </ul> 
-                    </div> 
-                : null }
-                <form >
-                    <div className="main--flex">
-                        <div>
-                            <label htmlFor="courseTitle">Course Title</label>
-                            <input 
-                            id="courseTitle" 
-                            name="courseTitle" 
-                            type="text" 
-                            value={title}
-                            onChange={(e) => setTitle(e.target.value)}
-                            />
-                            {/* User will have to be added dynamically after authorization is set up. */}
-                            <p>By: User</p>
-
-                            <label htmlFor="courseDescription">Course Description</label>
-                            <textarea 
-                            id="courseDescription" 
-                            name="courseDescription"
-                            type="text"
-                            value={description}
-                            onChange={(e) => setDescription(e.target.value)}
-                            />
-
-                            
-                        </div>
-                        <div>
-                        <label htmlFor="courseEstimatedTime">Estimated Time</label>
-                            <input
-                            id="courseEstimatedTime"
-                            name="courseEstimatedTime"
-                            type="text"
-                            value={estimatedTime}
-                            onChange={(e) => setEstimatedTime(e.target.value)}
-                            />
-
-                            <label htmlFor="courseMaterialsNeeded">Materials Needed</label>
-                            <input
-                            id="courseMaterialsNeeded"
-                            name="courseMaterialsNeeded"
-                            type="text"
-                            value={materialsNeeded}
-                            onChange={(e) => setMaterialsNeeded(e.target.value)}
-                            />
-                        </div>
-                    </div>
-                    <button className="button" type="submit" onClick={handleSubmit}>Create Course</button>
-                    <button className="button button-secondary">
-                    <Link to="/">Cancel</Link> </button>
-                    </form>  
-            </div>
+        <div className="wrap">
+            <h2>Create Course</h2>
+            <Form 
+            cancel={this.cancel}
+            errors={errors}
+            submit={this.submit}
+            submitButtonText="Create Course"
+            elements={() => (
+                <React.Fragment>
+                <input 
+                    id="title" 
+                    name="title" 
+                    type="text"
+                    value={title} 
+                    onChange={this.change} 
+                    placeholder="course title" />
+                    <input 
+                    id="description" 
+                    name="description" 
+                    type="textarea"
+                    value={description} 
+                    onChange={this.change} 
+                    placeholder="enter a description of the course" />
+                <input 
+                    id="estimatedTime" 
+                    name="estimatedTime" 
+                    type="text"
+                    value={estimatedTime} 
+                    onChange={this.change} 
+                    placeholder="estimated course length" />
+                <input 
+                    id="materialsNeeded" 
+                    name="materialsNeeded"
+                    type="text"
+                    value={materialsNeeded} 
+                    onChange={this.change} 
+                    placeholder="materials" />
+                </React.Fragment>
+            )} />
+        </div>
         )
     }
+
+    change = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+    
+        this.setState(() => {
+          return {
+            [name]: value
+          };
+        });
+      }
+    
+      submit = () => {
+        const { context } = this.props;
+        
+        const { 
+            title, 
+            description,
+            estimatedTime,
+            materialsNeeded
+      } = this.state
+
+      //Create a course that will get passed to context.data.createCourse and ultimately to the api.
+        const course = { 
+            title, 
+            description,
+            estimatedTime,
+            materialsNeeded
+        }
+
+        console.log('this.state: ', this.state);
+        console.log('course:', course);
+        console.log('this.props: ', this.props);
+        console.log('context: ', context);
+        
+   
+    
+    //Creates a new user using the createUser method in Data.js - user is passed as an argument and is the object  holds 
+    //the user's information.
+
+      context.data.createCourse(course)
+      .then( errors => {
+        if (errors.length) {
+          this.setState({ errors });
+        } else {
+          //If the response from Data.js returns no errors or an empty array, it means the course was created successfully.
+          console.log(`Course ${title} has been created!`);
+          };
+        })
+        .catch( err => { //handle rejected promises
+          console.log(err);
+          this.props.history.push('/error'); //push the error to the history stack and render the error page
+        });
+      }
+    
+      cancel = () => {
+        this.props.history.push('/'); //push the user back to the home page
+      }
+}
 
 
